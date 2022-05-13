840df647e43821fa430f5b4817f90018eed8cc56
Lee <leepotter010@gmail.com>
2022-05-14 07:47:06 +1000

    Conventional based routing for Movies Route

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -10,7 +10,13 @@ namespace Vidly.Controllers
         {
             var movie = new Movie() { Name = "Shrek!" };
 
-            return View(movie);
+            // return View(movie);
+            return Content("Hello World");
+        }
+
+        public IActionResult ByReleaseDate(int year, int month)
+        {
+            return Content(year + "/" + month);
         }
     }
 }
